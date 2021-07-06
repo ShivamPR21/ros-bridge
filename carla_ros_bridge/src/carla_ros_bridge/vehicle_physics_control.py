25a482e3cf190a44e1b0b6c6b78663cef563dbdd
Shivam Pandey <pandeyshivam2017robotics@gmail.com>
2021-07-06 13:26:23 +0530

    spawn process etension v1 with messaging process init.
    Signed-off-by: Shivam Pandey <pandeyshivam2017robotics@gmail.com>

--- carla_ros_bridge/src/carla_ros_bridge/vehicle_physics_control.py
+++ carla_ros_bridge/src/carla_ros_bridge/vehicle_physics_control.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright (c) 2021 BOSCH India
+# Copyright (c) 2021 Shivam Pandey
 #
 # This work is licensed under the terms of the MIT license.
 # For a copy, see <https://opensource.org/licenses/MIT>.
@@ -17,33 +17,6 @@ from carla_ros_bridge.sensor import Sensor
 from carla_msgs.msg import CarlaEgoVehicleControlWheel, \
     CarlaEgoVehicleControlGear, CarlaEgoVehicleControlPhysics
 
-
-class ControlVehiclePhysics():
-
-    def __init__(self,vehicle):
-        self.vehicle = vehicle
-        self.physics_control = vehicle.get_physics_control()
-
-    def CreateControl(self):   
-        # Create Wheels Physics Control
-        front_left_wheel  = carla.WheelPhysicsControl(tire_friction=4.5, damping_rate=1.0, max_steer_angle=70.0, radius=30.0)
-        front_right_wheel = carla.WheelPhysicsControl(tire_friction=2.5, damping_rate=1.5, max_steer_angle=70.0, radius=25.0)
-        rear_left_wheel   = carla.WheelPhysicsControl(tire_friction=1.0, damping_rate=0.2, max_steer_angle=0.0,  radius=15.0)
-        rear_right_wheel  = carla.WheelPhysicsControl(tire_friction=1.5, damping_rate=1.3, max_steer_angle=0.0,  radius=20.0)
-        wheels = [front_left_wheel, front_right_wheel, rear_left_wheel, rear_right_wheel]
-
-        self.physics_control.torque_curve = [carla.Vector2D(x=0, y=400), carla.Vector2D(x=1300, y=600)]
-        self.physics_control.max_rpm = 10000
-        self.physics_control.moi = 1.0
-        self.physics_control.damping_rate_full_throttle = 0.0
-        self.physics_control.use_gear_autobox = True
-        self.physics_control.gear_switch_time = 0.5
-        self.physics_control.clutch_strength = 10
-        self.physics_control.mass = 10000
-        self.physics_control.drag_coefficient = 0.25
-        self.physics_control.steering_curve = [carla.Vector2D(x=0, y=1), carla.Vector2D(x=100, y=1), carla.Vector2D(x=300, y=1)]
-        self.physics_control.wheels = wheels
-        self.physics_control.use_sweep_wheel_collision = True
 
 class PhysicsControl(PseudoActor):
     """
@@ -73,6 +46,7 @@ class PhysicsControl(PseudoActor):
                                                             self.get_topic_prefix() + "/set_physics",
                                                             self.on_physics_control,
                                                             qos_profile=10)
+        self.vehicle_physics_control = None
 
     def destroy(self):
         """
@@ -96,4 +70,105 @@ class PhysicsControl(PseudoActor):
 
     def on_physics_control(self, phy_ctrl):
         if self.parent and self.parent.carla_actor.is_alive:
+            # Create Physics Control Object for instance
+            self.vehicle_physics_control = self.parent.carla_actor.get_physics_control()
 
+            for wheel in phy_ctrl.wheels: 
+                wheel_ctrl = carla.WheelPhysicsControl(
+                    tire_friction = wheel.tire_friction,
+                    damping_rate = wheel.damping_rate,
+                    max_steer_angle = wheel.max_steer_angle,
+                    radius = wheel.radius,
+                    max_brake_torque = wheel.max_brake_torque,
+                    long_stiff_value = wheel.long_stiff_value,
+                    lat_stiff_max_load = wheel.lat_stiff_max_load,
+                    lat_stiff_value = wheel.lat_stiff_value
+                )
+
+                self.vehicle_physics_control.wheels[wheel.wheel_id] = wheel_ctrl
+
+            # update torque curve if provided with correct input size >= 2 points
+            if len(phy_ctrl.torque_curve) >= 2:
+                self.vehicle_physics_control.torque_curve = []
+                for trq_pt in phy_ctrl.torque_curve:
+                    # Construct required vector points and update torque curve
+                    self.vehicle_physics_control.torque_curve.append(carla.Vector2D(trq_pt.x, trq_pt.y))
+          
+            # update max_rpm if not 0(default)
+            if phy_ctrl.max_rpm != 0:
+                self.vehicle_physics_control.max_rpm = phy_ctrl.max_rpm
+
+            # update moi
+            if phy_ctrl.moi != 0:
+                self.vehicle_physics_control.moi = phy_ctrl.moi
+
+            # update damping_rate_full_throttle
+            if phy_ctrl.damping_rate_full_throttle != 0:
+                self.vehicle_physics_control.damping_rate_full_throttle = \
+                    phy_ctrl.damping_rate_full_throttle
+
+            # update damping_rate_zero_throttle_clutch_engaged
+            if phy_ctrl.damping_rate_zero_throttle_clutch_engaged != 0:
+                self.vehicle_physics_control.damping_rate_zero_throttle_clutch_engaged = \
+                    phy_ctrl.damping_rate_zero_throttle_clutch_engaged
+ 
+            # update damping_rate_zero_throttle_clutch_disengaged
+            if phy_ctrl.damping_rate_zero_throttle_clutch_disengaged != 0:
+                self.vehicle_physics_control.damping_rate_zero_throttle_clutch_disengaged = \
+                    phy_ctrl.damping_rate_zero_throttle_clutch_disengaged
+
+            # update use_gear_autobox
+            self.vehicle_physics_control.use_gear_autobox = \
+                phy_ctrl.use_gear_autobox
+
+            # update clutch_strength
+            if phy_ctrl.clutch_strength != 0:
+                self.vehicle_physics_control.clutch_strength = \
+                    phy_ctrl.clutch_strength
+
+            # update final_ratio
+            if phy_ctrl.final_ratio != 0:
+                self.vehicle_physics_control.final_ratio = \
+                    phy_ctrl.final_ratio
+
+            # update forward_gears
+            if len(phy_ctrl.forward_gears) != 0:
+                self.vehicle_physics_control.forward_gears = []
+                for gear in phy_ctrl.forward_gears:
+                    self.vehicle_physics_control.forward_gears.append(
+                        carla.GearPhysicsControl(ratio = gear.ratio, 
+                                                 down_ratio = gear.down_ratio, 
+                                                 up_ratio = gear.up_ratio
+                                                 )
+                    )
+                    
+            # update mass
+            if phy_ctrl.mass != 0:
+                self.vehicle_physics_control.mass = phy_ctrl.mass
+
+            # update drag_coefficient
+            if phy_ctrl.drag_coefficient != 0:
+                self.vehicle_physics_control.drag_coefficient = \
+                    phy_ctrl.drag_coefficient
+
+            # update center_of_mass
+            self.vehicle_physics_control.center_of_mass = \
+                carla.Vector3D(
+                    x = phy_ctrl.center_of_mass.x,
+                    y = phy_ctrl.center_of_mass.y,
+                    z = phy_ctrl.center_of_mass.z
+                )
+
+            # update steering_curve
+            if len(phy_ctrl.steering_curve) >= 2:
+                self.vehicle_physics_control.steering_curve = []
+                for steer_pt in phy_ctrl.steering_curve:
+                    # Construct required vector points and update torque curve
+                    self.vehicle_physics_control.steering_curve.append(carla.Vector2D(steer_pt.x, steer_pt.y))
+
+            # update use_sweep_wheel_collision
+            self.vehicle_physics_control.use_sweep_wheel_collision = \
+                phy_ctrl.use_sweep_wheel_collision
+
+            ## Apply Changes to carla_actor
+            self.parent.carla_actor.apply_physics_control(self.vehicle_physics_control)
